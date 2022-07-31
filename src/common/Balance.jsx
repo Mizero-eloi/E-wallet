769291908b9e0ef1a52dc89db0b232894aa6f506
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-07-31 22:19:25 +0200

    fised icons

--- src/common/Balance.jsx
+++ src/common/Balance.jsx
@@ -19,7 +19,31 @@ function Balance(props) {
             <div className="flex justify-between">
               {/* item  */}
               <div className="flex  w-[36%] align-center justify-center  mt-[7px]">
-                <div className="h-[30px] w-[30px] bg-yellow-300 rounded-lg mr-2 mt-[17px] "></div>
+                <div className="h-[30px] w-[30px] bg-[rgba(255,255,255,0.45)] rounded-lg mr-2 mt-[17px] flex justify-center items-center">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-[21px] h-[67px]"
+                  >
+                    <path
+                      d="M5 19L19 5"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M11 5H19V13"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
                 <span className="text-sm font-mono mt-[20px]">
                   {" "}
                   + $ 2,234.00{" "}
@@ -28,7 +52,31 @@ function Balance(props) {
 
               {/* item  */}
               <div className="flex  w-[36%] align-center justify-center  mt-[7px]  mr-20">
-                <div className="h-[30px] w-[30px] bg-yellow-300 rounded-lg mr-2 mt-[17px] "></div>
+                <div className="h-[30px] w-[30px] bg-[rgba(255,255,255,0.45)] rounded-lg mr-2 mt-[17px] flex justify-center items-center">
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-[21px] h-[67px]"
+                  >
+                    <path
+                      d="M19 5L5 19"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M13 19H5V11"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
                 <span className="text-sm font-mono mt-[20px]">
                   {" "}
                   - $ 2,234.00{" "}
--- src/common/CreditCard.jsx
+++ src/common/CreditCard.jsx
@@ -5,7 +5,64 @@ function CreditCard(props) {
     <div className="w-[59%] h-[130px] m-auto mt-[-83px] mb-[22px] rounded-xl bg-gray-700 bg-[url('../../images/2.png')] bg-no-repeat bg-cover p-3">
       {/* logo  */}
       <div>
-        <div className="h-[40px] w-[40px] bg-yellow-300 rounded-xl mr-2"></div>
+        <div className="h-[40px] w-[40px] bg-[rgba(255,255,255,0.45)] rounded-xl mr-2 flex justify-center items-center">
+          <svg
+            width="13"
+            height="10"
+            viewBox="0 0 13 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-[30px] h-[36px]"
+          >
+            <rect
+              x="5.78271"
+              y="1.73877"
+              width="0.394497"
+              height="7.8391"
+              fill="white"
+            />
+            <rect
+              x="0.767578"
+              y="2.25867"
+              width="0.509478"
+              height="5.01521"
+              transform="rotate(-90 0.767578 2.25867)"
+              fill="white"
+            />
+            <rect
+              x="0.767578"
+              y="4.54175"
+              width="0.509478"
+              height="5.01521"
+              transform="rotate(-90 0.767578 4.54175)"
+              fill="white"
+            />
+            <rect
+              x="7.68677"
+              y="9.57788"
+              width="0.394497"
+              height="8.9114"
+              transform="rotate(180 7.68677 9.57788)"
+              fill="white"
+            />
+            <rect
+              x="12.7021"
+              y="4.86755"
+              width="0.509478"
+              height="5.01521"
+              transform="rotate(90 12.7021 4.86755)"
+              fill="white"
+            />
+            <rect
+              x="12.7021"
+              y="2.8783"
+              width="0.509478"
+              height="5.01521"
+              transform="rotate(90 12.7021 2.8783)"
+              fill="white"
+            />
+          </svg>
+        </div>
       </div>
 
       {/* the wallet id */}
