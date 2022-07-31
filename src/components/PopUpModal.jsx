ce8db21a6c50d268e8b24b235275f10ff3fe615c
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-07-31 22:32:03 +0200

    fixed icons 2

--- src/components/PopUpModal.jsx
+++ src/components/PopUpModal.jsx
@@ -8,7 +8,28 @@ function PopUpModal({ toggleModal }) {
           className="text-white relative float-right mr-5 mt-4 cursor-pointer"
           onClick={toggleModal}
         >
-          close
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18 18L6 6"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M18 6L5.99997 18"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
         </h2>
         <div className="w-[40%] h-30 bg-white m-auto p-[31px] mt-[68px] rounded-sm">
           <h2 className="w-[12%] m-auto font-semibold text-2xl">Transfer</h2>
