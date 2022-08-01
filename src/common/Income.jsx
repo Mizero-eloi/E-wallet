40bfbaa89bc945922791a803859a03866938d123
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-01 02:49:52 +0200

    income & context

--- src/common/Income.jsx
+++ src/common/Income.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { GlobalContext } from "../context/GlobalState";
 
 function Income(props) {
+  const { transactions } = useContext(GlobalContext);
+  const incomes = transactions.filter((transaction) => transaction.amount > 0);
+
   return (
-    <div className="w-full h-[151px] rounded-2xl float-right  bg-white  bg-no-repeat bg-cover mt-5 p-[15px]">
-      <p className="text-gray-900 font-semibold">Income</p>
+    <div
+      className={
+        incomes.length === 0
+          ? "w-full h-[151px] rounded-2xl float-right  bg-white  bg-no-repeat bg-cover mt-5 p-[15px]"
+          : "w-full rounded-2xl float-right  bg-white  bg-no-repeat bg-cover mt-5 p-[15px]"
+      }
+    >
+      <div className="text-gray-900 font-semibold flex justify-between">
+        <p className="w-[15%]">Income</p>
+        <Link to="/allIncomes" className="w-[15%] relative right-[68px]">
+          See all
+        </Link>
+      </div>
 
       {/* Income list  */}
       <div>
-        {/* Income item  */}
-        <div className="mt-4 ">
-          <div className="flex font-semibold">
-            <h2 className=" text-gray-800 w-[75%]">Salary</h2>{" "}
-            <span className="relative right-[56px]">$ 5,000.00 </span>
+        {incomes.length === 0 && (
+          <h2 className="w-[32%] m-auto mt-[30px] font-semibold">
+            No incomes yet
+          </h2>
+        )}
+
+        {/* income item  */}
+        {incomes.map((t) => (
+          <div className="mt-4 ">
+            <div className="flex font-semibold">
+              <h2 className=" text-gray-800 w-[75%]">Salary</h2>{" "}
+              <span className="relative right-[56px]">$ 5,000.00 </span>
+            </div>
+            <p className="mt-2 text-gray-700">
+              Lorem, ipsum dolor sit amet consectetur
+            </p>
+            {/* border line  */}
+            <div className="w-[75%] h-[2px] bg-gray-400 mt-[15px]"></div>
           </div>
-          <p className="mt-2 text-gray-700">
-            Lorem, ipsum dolor sit amet consectetur
-          </p>
-          {/* border line  */}
-          <div className="w-[75%] h-[2px] bg-gray-400 mt-[15px]"></div>
-        </div>
+        ))}
+        {/* Income item  */}
       </div>
     </div>
   );
