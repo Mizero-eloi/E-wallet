1bad21c87383562c565fb8d1c5a26afea628a877
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-01 05:46:48 +0200

    Table component

---
+++ src/common/Table.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Table({ columns, row }) {
+  return (
+    <table className="min-w-full">
+      <thead className="border-b bg-gray-900 ">
+        <tr>
+          {columns.map((c) => (
+            <th
+              scope="col"
+              className="text-sm font-medium text-white px-6 py-4 text-left"
+            >
+              {c}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {row.map((data) => (
+          <tr className="border-b bg-white p-[30px]">
+            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+              1
+            </td>
+            <td className="text-sm text-gray-900 font-light px-6 py-6 whitespace-nowrap">
+              {data.label}
+            </td>
+            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {data.amount}
+            </td>
+            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {data.description}
+            </td>
+            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+              {data.date}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- src/components/AllExpenses.jsx
+++ src/components/AllExpenses.jsx
@@ -5,6 +5,7 @@ import PopUpModal from "../common/PopUpModal";
 import Form from "../common/Form";
 import { GlobalContext } from "../context/GlobalState";
 import Button from "../common/Button";
+import Table from "../common/Table";
 
 function AllExpenses(props) {
   const [isViewModal, setIsViewModal] = useState(false);
@@ -43,63 +44,10 @@ function AllExpenses(props) {
             <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                 <div className="overflow-hidden">
-                  <table className="min-w-full">
-                    <thead className="border-b bg-gray-900 ">
-                      <tr>
-                        <th
-                          scope="col"
-                          className="text-sm font-medium text-white px-6 py-4 text-left"
-                        >
-                          N<sup className="underline">o</sup>
-                        </th>
-                        <th
-                          scope="col"
-                          className="text-sm font-medium text-white px-6 py-6 text-left"
-                        >
-                          Label
-                        </th>
-                        <th
-                          scope="col"
-                          className="text-sm font-medium text-white px-6 py-4 text-left"
-                        >
-                          Amount
-                        </th>
-                        <th
-                          scope="col"
-                          className="text-sm font-medium text-white px-6 py-4 text-left"
-                        >
-                          Description
-                        </th>
-                        <th
-                          scope="col"
-                          className="text-sm font-medium text-white px-6 py-4 text-left"
-                        >
-                          Date
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {expenses.map((expense) => (
-                        <tr className="border-b bg-white p-[30px]">
-                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                            1
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-6 whitespace-nowrap">
-                            {expense.label}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {expense.amount}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {expense.description}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {expense.date}
-                          </td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                  <Table
+                    columns={["No", "Label", "Amount", "Description", "Date"]}
+                    row={expenses}
+                  />
                 </div>
               </div>
             </div>
